5027d559e1366ef1953ca1d5c383a8e5d008fa91
TonyJin99 <Tony@TonyJin.local>
2016-07-23 15:18:20 -0400

    Initialize

---
+++ SinaWeibo/SinaWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinaWeibo
+//
+//  Created by Tony.Jin on 7/23/16.
+//  Copyright © 2016 Innovatis Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
